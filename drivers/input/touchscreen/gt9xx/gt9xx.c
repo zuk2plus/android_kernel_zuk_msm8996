6858e9f6831ad9b6d3670a85f724bf26bda0475e
Silentlys <1561707299@qq.com>
2016-09-07 15:41:29 +0800

    update

--- drivers/input/touchscreen/gt9xx/gt9xx.c
+++ drivers/input/touchscreen/gt9xx/gt9xx.c
@@ -744,9 +744,9 @@ static void goodix_ts_work_func(struct work_struct *work)
                 gtp_i2c_write(i2c_connect_client, doze_buf, 3);
 				doze_status = DOZE_WAKEUP;
                 GTP_INFO("Double click to light up the screen!");
-                input_report_key(ts->input_dev, KEY_WAKEUP, 1);
+                input_report_key(ts->input_dev, KEY_GESTURE_DT, 1);
                 input_sync(ts->input_dev);
-                input_report_key(ts->input_dev, KEY_WAKEUP, 0);
+                input_report_key(ts->input_dev, KEY_GESTURE_DT, 0);
                 input_sync(ts->input_dev);
             }
             else
@@ -2280,7 +2280,7 @@ static s8 gtp_request_input_dev(struct goodix_ts_data *ts)
 	input_set_capability(ts->input_dev, EV_KEY, KEY_GESTURE_LR);
 	input_set_capability(ts->input_dev, EV_KEY, KEY_GESTURE_RL);
 	input_set_capability(ts->input_dev, EV_KEY, KEY_GESTURE_DT);
-    input_set_capability(ts->input_dev, EV_KEY, KEY_WAKEUP);
+    input_set_capability(ts->input_dev, EV_KEY, KEY_POWER);
 #endif 
 
 #if GTP_CHANGE_X2Y
--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -253,7 +253,6 @@ enum mdss_intf_events {
 	MDSS_EVENT_DSI_RECONFIG_CMD,
 	MDSS_EVENT_DSI_RESET_WRITE_PTR,
 	MDSS_EVENT_PANEL_TIMING_SWITCH,
-	MDSS_EVENT_UPDATE_LIVEDISPLAY,
 };
 
 struct lcd_panel_info {
@@ -535,8 +534,6 @@ struct mdss_mdp_pp_tear_check {
 	u32 refx100;
 };
 
-struct mdss_livedisplay_ctx;
-
 struct mdss_panel_info {
 	u32 xres;
 	u32 yres;
@@ -665,8 +662,6 @@ struct mdss_panel_info {
 	 * configuring the event timer wakeup logic.
 	 */
 	u32 adjust_timer_delay_ms;
-
-	struct mdss_livedisplay_ctx *livedisplay;
 
 	/* debugfs structure for the panel */
 	struct mdss_panel_debugfs_info *debugfs_info;
